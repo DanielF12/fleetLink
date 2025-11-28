ee52a5db12d8670f47bca5fd73d72b840eb43ac3
DanielF12 <danielfeelipe012@gmail.com>
2025-11-27 21:54:15 -0300

    add tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HM
+        <p className="text-3xl font-bold">
+          Olá, Tailwind V4
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
